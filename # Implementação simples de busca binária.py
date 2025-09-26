af49d2ffeeaef4ee69325a064df6237f357c10c4
fernanda <fernandaferreira140407@gmail.com>
2025-09-26 14:29:35 -0300

    first commit

---
+++ # Implementação simples de busca binária.py
@@ -0,0 +1,21 @@
+# Implementação simples de busca binária
+
+def busca_binaria(lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+
+    while baixo <= alto:
+        meio = (baixo + alto) // 2
+        chute = lista[meio]
+
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+    return None
+
+numeros = [1, 3, 5, 7, 9, 11]
+print("Lista:", numeros)
+print("Posição do número 7:", busca_binaria(numeros, 7))
